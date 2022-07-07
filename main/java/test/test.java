911467c24e94e317399cced684d83ddeb55331d5
hopels <101801438+hopels@users.noreply.github.com>
2022-07-07 09:48:57 +0900

    Update test.java
    test

--- main/java/test/test.java
+++ main/java/test/test.java
@@ -3,7 +3,7 @@ package test;
 public class test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub/test
 		System.out.println("TEST 파일 입니다.");
 	}
 
