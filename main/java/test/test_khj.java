d9023e1f4bbfc0c9193c9b467649bcc82e531103
StWK <d89745@gmail.com>
2022-07-07 10:13:53 +0900

    Update test_khj.java
    test_khj edit

--- main/java/test/test_khj.java
+++ main/java/test/test_khj.java
@@ -4,7 +4,10 @@ public class test_khj {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("김홍준 테스트");
+		int num01 = 10;
+		int num02 = 20;
+		System.out.println("김홍준 테스트: "+(num01+num02));
+		
 	}
 
 }
