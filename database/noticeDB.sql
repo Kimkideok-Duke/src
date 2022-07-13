b1fac4f2c33fe2dfafe3af01eab54884a7675c0a
YJoooo <107245938+YJoooo@users.noreply.github.com>
2022-07-13 18:19:09 +0900

    notice DB

--- database/noticeDB.sql
+++ database/noticeDB.sql
@@ -1,3 +1,18 @@
 CREATE TABLE notice(
-	
+	noticeno NUMBER PRIMARY KEY,
+	pno NUMBER,
+    FOREIGN KEY (pno)
+    REFERENCES PROJECT (pno),
+	title varchar2(100) NOT NULL,
+	content varchar2(2000) NOT NULL,
+	creatdate DATE NOT NULL,
+	readcnt NUMBER NOT NULL
 );
+SELECT * FROM notice;
+CREATE SEQUENCE notice_seq
+	START WITH 1
+	MINVALUE 1
+	MAXVALUE 999999
+	INCREMENT BY 1;
+
+INSERT INTO notice VALUES(notice_seq.nextval, ?,'?', '?', sysdate, 0);
